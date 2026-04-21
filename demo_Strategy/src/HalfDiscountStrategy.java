57cfc5678c5b57cf36d992f3f98eaaa87af14847
CSerVN <summerthinh3@gmail.com>
2026-04-21 21:01:46 +0700

    initial push

---
+++ demo_Strategy/src/HalfDiscountStrategy.java
@@ -0,0 +1,10 @@
+
+public class HalfDiscountStrategy implements IPromoteStrategy {
+
+	@Override
+	public double doDiscountStrategy(double price) {
+		// TODO Auto-generated method stub
+		return price*0.5;
+	}
+
+}
